9f0d7e558947384211717e4cd500c0121aa35242
RAMESSESII2 <stmbhardwaj@gmail.com>
2020-03-08 04:42:23 +0000

    till comments

---
+++ app.js
@@ -0,0 +1,117 @@
+const express= require('express');
+const app= express();
+const request=require('request');
+const bodyparser=require('body-parser');
+const mongoose=require('mongoose');
+const Campground=require("./models/campgrounds.js");
+const Comment=require("./models/comments.js");
+mongoose.connect("mongodb://localhost:27017/yelpcampdb",{useUnifiedTopology: true});
+const seedDB=require("./seeds.js");
+app.use(express.static(__dirname+"/public"));
+seedDB();
+// const campgroundSchema= new mongoose.Schema({
+// 	name: String,
+// 	image: String,
+// 	description: String
+// });
+// Campground= mongoose.model("Campground", campgroundSchema);
+
+
+/*	const sites=
+		
+		,
+				
+		,
+		{
+			name: "Prayagraj",
+			image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+		},
+		{
+			name: "Yamunotri",
+			image:"https://images.unsplash.com/photo-1539183204366-63a0589187ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+		}	
+	]; */
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.get('/',(req, res)=>{
+	res.render("campgrounds/home");
+});
+
+app.get('/campgrounds',(req, res)=>{
+	
+	Campground.find({}, function(err, allcampgrounds){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("campgrounds/yelpgrounds", {sites: allcampgrounds});
+		}
+	});
+	
+});
+
+app.post('/campgrounds',(req,res)=>{
+	const name=req.body.name;
+	const link=req.body.link;
+	const desc= req.body.description;
+	const newcamp={name:name, image: link, description: desc};
+	Campground.create(newcamp, function(err, newlycreated){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.redirect("/campgrounds");
+		}
+	})
+	
+});
+
+app.get('/campgrounds/new',(req,res)=>{
+	res.render("campgrounds/newGrounds");
+});
+
+app.get('/campgrounds/:id',(req, res)=>{
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log(foundCampground);
+			res.render("campgrounds/show", {campground : foundCampground});
+		}
+	});
+});
+//COMMENT ROUTES=========================
+
+app.get("/campgrounds/:id/comments/new",function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err)
+			console.log(err);
+		else
+			res.render("comments/new",{campground: campground});			
+	});
+
+});
+
+app.post("/campgrounds/:id/comments",function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+			res.redirect("/campgrounds");}
+		else{
+		Comment.create(req.body.comment,async function(err, comment){
+				if(err)
+					console.log(err);
+				else{						await campground.comments.push(comment);
+					 console.log("Comment added ");
+					await campground.save();
+					 console.log(comment);
+					res.redirect('/campgrounds/'+campground._id);}
+			});		
+	};
+});
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+	console.log('Movie App Server is online');
+});
---
+++ seeds.js
@@ -0,0 +1,48 @@
+const mongoose= require("mongoose"),
+	  Campground=require("./models/campgrounds.js"),
+	  Comment=require("./models/comments"),
+	  seeds = [
+		  {
+		 name:"Heaven",
+		 image:"https://phoenixrvpark.com/wp-content/uploads/Best-Car-Camping-Gear.jpg",
+		 description:"Diam in arcu cursus euismod quis viverra nibh. Viverra nibh cras pulvinar mattis nunc sed blandit libero. Rutrum quisque non tellus orci ac auctor augue. Gravida arcu ac tortor dignissim convallis aenean et tortor. Et pharetra pharetra massa massa ultricies mi. Vehicula ipsum a arcu cursus vitae. Massa tempor nec feugiat nisl pretium fusce id velit. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Sollicitudin tempor id eu nisl nunc mi ipsum. Urna nunc id cursus metus aliquam eleifend mi in."
+		  },{
+		  name:"Sunshine",
+ image:"https://www.reserveamerica.com/webphotos/racms/articles/images/fef91bb3-1dff-444d-b0e5-d14db129ce1d_image2_0-main-tent.jpg",
+		description:"Diam in arcu cursus euismod quis viverra nibh. Viverra nibh cras pulvinar mattis nunc sed blandit libero. Rutrum quisque non tellus orci ac auctor augue. Gravida arcu ac tortor dignissim convallis aenean et tortor. Et pharetra pharetra massa massa ultricies mi. Vehicula ipsum a arcu cursus vitae. Massa tempor nec feugiat nisl pretium fusce id velit. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Sollicitudin tempor id eu nisl nunc mi ipsum. Urna nunc id cursus metus aliquam eleifend mi in."
+		  },
+		   {
+		 name:"Heaven",
+		 image:"https://phoenixrvpark.com/wp-content/uploads/Best-Car-Camping-Gear.jpg",
+		 description:"Diam in arcu cursus euismod quis viverra nibh. Viverra nibh cras pulvinar mattis nunc sed blandit libero. Rutrum quisque non tellus orci ac auctor augue. Gravida arcu ac tortor dignissim convallis aenean et tortor. Et pharetra pharetra massa massa ultricies mi. Vehicula ipsum a arcu cursus vitae. Massa tempor nec feugiat nisl pretium fusce id velit. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Sollicitudin tempor id eu nisl nunc mi ipsum. Urna nunc id cursus metus aliquam eleifend mi in."
+		  },{
+		  name:"Sunshine",
+ image:"https://www.reserveamerica.com/webphotos/racms/articles/images/fef91bb3-1dff-444d-b0e5-d14db129ce1d_image2_0-main-tent.jpg",
+		description:"Blah Blah Blah Blah"
+		  }
+	  ];
+async function seedDB(){
+	try{
+		await Campground.remove({});
+		console.log('Campgrounds removed');
+		await Comment.remove({});
+
+		for(const seed of seeds){
+				//add few campgrounds
+		let campground=await Campground.create(seed);
+		console.log('Campgrounds created');
+		let comment=await Comment.create({
+					text:"This is a great place to visit",
+				author:"Holmes"});
+		console.log('Comments created');	
+		campground.comments.push(comment);
+		campground.save();
+		console.log('Comments added to campgrounds');
+	};
+} 	catch(err){
+		console.log(err);
+	}
+};
+	
+
+module.exports=seedDB;
